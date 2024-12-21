64a1172295c0da5435f0584e515f3238853d32c1
Hasith Udayanga <uhasith5@gmail.com>
2024-12-21 12:59:28 +0530

    setup powergrid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import wireuiConfig from "./vendor/wireui/wireui/tailwind.config.js";
+import powerGridConfig from "./vendor/power-components/livewire-powergrid/tailwind.config.js";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [wireuiConfig],
+    presets: [wireuiConfig, powerGridConfig],
+    darkMode: "class",
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -13,6 +15,9 @@ export default {
         "./vendor/wireui/wireui/ts/**/*.ts",
         "./vendor/wireui/wireui/src/WireUi/**/*.php",
         "./vendor/wireui/wireui/src/Components/**/*.php",
+        "./app/Livewire/**/*Table.php",
+        "./vendor/power-components/livewire-powergrid/resources/views/**/*.php",
+        "./vendor/power-components/livewire-powergrid/src/Themes/Tailwind.php",
     ],
 
     theme: {
@@ -20,6 +25,9 @@ export default {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
+        },
+        colors: {
+            "pg-primary": colors.gray,
         },
     },
 
